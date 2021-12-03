93ff8059c96ad487560158c9229d6478fdfdff71
Azarolol <gimazovazamat@mail.ru>
2021-12-03 23:54:07 +0300

    Scripted auto-commit on change (2021-12-03 23:54:07) by gitwatch.sh

--- src/main/java/hdfs/lab4/azarolol/JSTester.java
+++ src/main/java/hdfs/lab4/azarolol/JSTester.java
@@ -2,20 +2,36 @@ package hdfs.lab4.azarolol;
 
 import akka.NotUsed;
 import akka.actor.ActorSystem;
+import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
 
+import java.io.IOException;
+import java.util.concurrent.CompletionStage;
+
 public class JSTester {
     final static String ACTOR_SYSTEM_NAME = "ActorsRoutes";
-    public static void main (String[] args) {
+    final static String HOST = "localhost";
+    final static int PORT = 8080;
+    public static void main (String[] args) throws IOException {
         ActorSystem system = ActorSystem.create(ACTOR_SYSTEM_NAME);
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
         JSTester instance = new JSTester();
         final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = instance.createRoute(system).flow(system, materializer);
-
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
+                routeFlow,
+                ConnectHttp.toHost(HOST, PORT),
+                materializer
+        );
+        System.out.println("Server online at http://" + HOST + ":" + PORT + "/\nPress RETURN to stop...");
+        System.in.read();
+        binding
+                .thenCompose(ServerBinding::unbind)
+                .thenAccept(unbound -> system.terminate());
     }
 }
