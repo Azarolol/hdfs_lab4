8ca565ea539843236f854e5d07ec9ee1b8c4b4fa
Azarolol <gimazovazamat@mail.ru>
2021-12-04 00:38:12 +0300

    Scripted auto-commit on change (2021-12-04 00:38:12) by gitwatch.sh

--- src/main/java/hdfs/lab4/azarolol/Test.java
+++ src/main/java/hdfs/lab4/azarolol/Test.java
@@ -1,9 +1,31 @@
 package hdfs.lab4.azarolol;
 
-public class Test {
-    private static String testName;
-    private static String expectedResult;
-    private static Object[] params;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
-    
+public class Test {
+    private final String testName;
+    private final String expectedResult;
+    private final Object[] params;
+
+    @JsonCreator
+    public Test(@JsonProperty("testName") String testName,
+                @JsonProperty("expectedResult") String expectedResult,
+                @JsonProperty("params") Object[] params) {
+        this.testName = testName;
+        this.expectedResult = expectedResult;
+        this.params = params;
+    }
+
+    public String getExpectedResult() {
+        return expectedResult;
+    }
+
+    public String getTestName() {
+        return testName;
+    }
+
+    public Object[] getParams() {
+        return params;
+    }
 }
--- src/main/java/hdfs/lab4/azarolol/TestPackageMessage.java
+++ src/main/java/hdfs/lab4/azarolol/TestPackageMessage.java
@@ -4,5 +4,5 @@ public class TestPackageMessage {
     private final String packageID;
     private final String jsScript;
     private final String functionName;
-
+    private final 
 }
