1c39dbbf0d596b195cce0223a6741afe64ead531
Azarolol <gimazovazamat@mail.ru>
2021-12-04 01:53:08 +0300

    Scripted auto-commit on change (2021-12-04 01:53:08) by gitwatch.sh

--- src/main/java/hdfs/lab4/azarolol/Router.java
+++ src/main/java/hdfs/lab4/azarolol/Router.java
@@ -3,13 +3,16 @@ package hdfs.lab4.azarolol;
 import akka.actor.AbstractActor;
 
 public class Router extends AbstractActor {
+
     @Override
     public Receive createReceive() {
         return receiveBuilder()
                 .match(
                         TestPackageMessage.class,
                         message -> {
-                            
+                            for (Test test: message.getTests()) {
+
+                            }
                         }
                 )
     }
