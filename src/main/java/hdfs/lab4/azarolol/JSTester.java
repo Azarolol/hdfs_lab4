8f0352187c43c380ef7fe1a5f3765823067e436a
Azarolol <gimazovazamat@mail.ru>
2021-12-03 23:46:23 +0300

    Scripted auto-commit on change (2021-12-03 23:46:23) by gitwatch.sh

--- src/main/java/hdfs/lab4/azarolol/JSTester.java
+++ src/main/java/hdfs/lab4/azarolol/JSTester.java
@@ -1,11 +1,13 @@
 package hdfs.lab4.azarolol;
 
 import akka.actor.ActorSystem;
+import akka.http.javadsl.Http;
 
 public class JSTester {
     final static String ACTOR_SYSTEM_NAME = "ActorsRoutes";
     public static void main (String[] args) {
         ActorSystem system = ActorSystem.create(ACTOR_SYSTEM_NAME);
-        
+        final Http http = Http.get(system);
+
     }
 }
