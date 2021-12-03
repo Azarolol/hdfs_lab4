461c9d2d544f650333c2e1b897c45be90fc2fa50
Azarolol <gimazovazamat@mail.ru>
2021-12-04 00:29:04 +0300

    Scripted auto-commit on change (2021-12-04 00:29:04) by gitwatch.sh

--- src/main/java/hdfs/lab4/azarolol/TestPackageMessage.java
+++ src/main/java/hdfs/lab4/azarolol/TestPackageMessage.java
@@ -2,5 +2,7 @@ package hdfs.lab4.azarolol;
 
 public class TestPackageMessage {
     private final String packageID;
-    private final String 
+    private final String jsScript;
+    private final String functionName;
+    
 }
