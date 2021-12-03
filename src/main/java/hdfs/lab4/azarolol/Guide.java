d8332d9e607054b97f020537fcd805a1b2bbabfb
Azarolol <gimazovazamat@mail.ru>
2021-12-04 01:56:26 +0300

    Scripted auto-commit on change (2021-12-04 01:56:26) by gitwatch.sh

--- src/main/java/hdfs/lab4/azarolol/Guide.java
+++ src/main/java/hdfs/lab4/azarolol/Guide.java
@@ -1,10 +1,13 @@
 package hdfs.lab4.azarolol;
 
 import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
+import akka.routing.RoundRobinRoutingLogic;
 import akka.routing.Router;
 
 public class Guide extends AbstractActor {
-    Router router = new Router(Rou)
+    ActorRef keeper = 
+    Router router = new Router(new RoundRobinRoutingLogic());
     @Override
     public Receive createReceive() {
         return receiveBuilder()
