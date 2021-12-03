2b251c91c7c219a1a17b6d0f09d798e595d1cbc0
Azarolol <gimazovazamat@mail.ru>
2021-12-04 02:04:27 +0300

    Scripted auto-commit on change (2021-12-04 02:04:27) by gitwatch.sh

--- src/main/java/hdfs/lab4/azarolol/Guide.java
+++ src/main/java/hdfs/lab4/azarolol/Guide.java
@@ -3,12 +3,26 @@ package hdfs.lab4.azarolol;
 import akka.actor.AbstractActor;
 import akka.actor.ActorRef;
 import akka.actor.Props;
+import akka.routing.ActorRefRoutee;
 import akka.routing.RoundRobinRoutingLogic;
+import akka.routing.Routee;
 import akka.routing.Router;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Guide extends AbstractActor {
+    final static int NUMBER_OF_TESTERS = 3;
+
     ActorRef keeper = getContext().actorOf(Props.create(Keeper.class));
-    Router router = new Router(new RoundRobinRoutingLogic());
+    List<Routee> routes= new ArrayList<>();
+    {
+        for (int i = 0; i < NUMBER_OF_TESTERS; i++) {
+            ActorRef tester = getContext().actorOf(Props.create(Tester.class));
+            routes.add(new ActorRefRoutee(tester));
+        }
+    }
+    Router router = new Router(new RoundRobinRoutingLogic(), routes);
     @Override
     public Receive createReceive() {
         return receiveBuilder()
@@ -16,9 +30,14 @@ public class Guide extends AbstractActor {
                         TestPackageMessage.class,
                         message -> {
                             for (Test test: message.getTests()) {
-
+                                router.route(test, keeper);
                             }
                         }
                 )
+                .match(
+                        GetResultMessage.class,
+                        message -> keeper.tell(message, sender())
+                )
+                .build();
     }
 }
