1213dc056c0ad80fa9c6367bce21fb16d734934e
Azarolol <gimazovazamat@mail.ru>
2021-12-04 00:49:32 +0300

    Scripted auto-commit on change (2021-12-04 00:49:32) by gitwatch.sh

--- src/main/java/hdfs/lab4/azarolol/Keeper.java
+++ src/main/java/hdfs/lab4/azarolol/Keeper.java
@@ -1,4 +1,10 @@
 package hdfs.lab4.azarolol;
 
-public class Keeper {
+import akka.actor.AbstractActor;
+
+public class Keeper extends AbstractActor {
+    @Override
+    public Receive createReceive() {
+        return null;
+    }
 }
