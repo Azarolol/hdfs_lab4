215c82178675339a72de3b1ee1e9cf2529fb9a66
Azarolol <gimazovazamat@mail.ru>
2021-12-04 02:10:29 +0300

    Scripted auto-commit on change (2021-12-04 02:10:29) by gitwatch.sh

--- src/main/java/hdfs/lab4/azarolol/JSTester.java
+++ src/main/java/hdfs/lab4/azarolol/JSTester.java
@@ -3,6 +3,7 @@ package hdfs.lab4.azarolol;
 import akka.NotUsed;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
+import akka.actor.Props;
 import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
 import akka.http.javadsl.ServerBinding;
@@ -10,8 +11,10 @@ import akka.http.javadsl.marshallers.jackson.Jackson;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
 import akka.http.javadsl.server.Route;
+import akka.pattern.Patterns;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
+import scala.concurrent.Future;
 
 import java.io.IOException;
 import java.util.concurrent.CompletionStage;
@@ -25,12 +28,14 @@ public class JSTester {
     final static String WELCOME_MESSAGE = "Server online at http://" + HOST + ":" + PORT + "/\nPress RETURN to stop...";
     final static String TEST_STARTED_MESSAGE = "Test started!";
     final static String TEST_SEGMENT = "test";
+    final static String RESULT_SEGMENT = "result";
     public static void main (String[] args) throws IOException {
         ActorSystem system = ActorSystem.create(ACTOR_SYSTEM_NAME);
+        ActorRef guide = system.actorOf(Props.create(Guide.class));
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
         JSTester instance = new JSTester();
-        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = instance.createRoute(system).flow(system, materializer);
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = instance.createRoute(guide).flow(system, materializer);
         final CompletionStage<ServerBinding> binding = http.bindAndHandle(
                 routeFlow,
                 ConnectHttp.toHost(HOST, PORT),
@@ -43,13 +48,30 @@ public class JSTester {
                 .thenAccept(unbound -> system.terminate());
     }
 
-    private Route createRoute(ActorSystem system) {
+    private Route createRoute(ActorRef guide) {
         return route(
                 path(TEST_SEGMENT, () ->
-                        post(() ->
-                            entity(Jackson.unmarshaller(TestPackageMessage.class), msg -> {
-                                testPackageActor.tell(msg, ActorRef.noSender());
-                                return complete(TEST_STARTED_MESSAGE);
-                            }))));
+                        route(
+                            post(() ->
+                                entity(Jackson.unmarshaller(TestPackageMessage.class), msg -> {
+                                    guide.tell(msg, ActorRef.noSender());
+                                    return complete(TEST_STARTED_MESSAGE);
+                                })))),
+                path(RESULT_SEGMENT, () ->
+                        route(
+                                get(
+                                        () -> parameter("packageId",
+                                                (id) -> {
+                                            Future<Object> result = Patterns.ask(
+                                                    guide,
+                                                    new GetResultMessage(id),
+                                                    5000
+                                            );
+                                            return completeOKWithFuture(result, Jackson.marshaller());
+                                        })
+                                )
+                        )
+                )
+        );
     }
 }
