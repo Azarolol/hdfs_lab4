92f8102c299c62802bf17e1aacc22efe3223a2ad
Azarolol <gimazovazamat@mail.ru>
2021-12-04 00:40:42 +0300

    Scripted auto-commit on change (2021-12-04 00:40:42) by gitwatch.sh

--- src/main/java/hdfs/lab4/azarolol/TestPackageMessage.java
+++ src/main/java/hdfs/lab4/azarolol/TestPackageMessage.java
@@ -1,9 +1,21 @@
 package hdfs.lab4.azarolol;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class TestPackageMessage {
     private final String packageID;
     private final String jsScript;
     private final String functionName;
     private final Test[] tests;
-    
+
+    @JsonCreator
+    public TestPackageMessage(
+            @JsonProperty("packageId") String packageID,
+            @JsonProperty("jsScript") String jsScript,
+            @JsonProperty("functionName") String functionName,
+            @JsonProperty("tests") Test[] tests) {
+        
+    }
+    )
 }
