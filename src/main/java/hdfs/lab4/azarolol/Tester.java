ce6f99759f4180ccc2401875580766201b3a503f
Azarolol <gimazovazamat@mail.ru>
2021-12-04 01:39:51 +0300

    Scripted auto-commit on change (2021-12-04 01:39:51) by gitwatch.sh

--- src/main/java/hdfs/lab4/azarolol/Tester.java
+++ src/main/java/hdfs/lab4/azarolol/Tester.java
@@ -5,6 +5,18 @@ import akka.actor.AbstractActor;
 public class Tester extends AbstractActor {
     @Override
     public Receive createReceive() {
+        return receiveBuilder()
+                .match(
+                        Test.class,
+                        message -> sender().tell(
+                                runTest(message),
+                                self()
+                        )
+                )
+                .build();
+    }
+
+    private TestResult runTest(Test test) {
         
     }
 }
