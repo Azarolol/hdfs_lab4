bf91daf889f3a42c64dfaccca4a9be8a5d826599
Azarolol <gimazovazamat@mail.ru>
2021-12-04 13:49:18 +0300

    Scripted auto-commit on change (2021-12-04 13:49:18) by gitwatch.sh

--- src/main/java/hdfs/lab4/azarolol/TestResult.java
+++ src/main/java/hdfs/lab4/azarolol/TestResult.java
@@ -3,8 +3,11 @@ package hdfs.lab4.azarolol;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.Objects;
+
 public class TestResult {
     private final String testName;
+    private final boolean passed;
     private final String expectedResult;
     private final String receivedResult;
 
@@ -13,6 +16,7 @@ public class TestResult {
                       @JsonProperty ("expectedResult") String expectedResult,
                       @JsonProperty("receivedResult") String receivedResult) {
         this.testName = testName;
+        this.passed = Objects.equals(expectedResult, receivedResult);
         this.expectedResult = expectedResult;
         this.receivedResult = receivedResult;
     }
